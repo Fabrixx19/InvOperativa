2a102da03505f67ad692b790ad516431736b8abe
sampi74 <facusampieri74@gmail.com>
2024-06-03 20:38:06 -0300

    fg

--- invOperativa/stock/models.py
+++ invOperativa/stock/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Articulo(models.Model):
+    codArticulo = models.AutoField(primary_key=True)
     fechaAltaArticulo = models.DateTimeField(auto_now_add=False) 
     fechaBajaArticulo = models.DateTimeField()
+    nombreArticulo = models.TextField(max_length=30)
+    stockArticulo = models.IntegerField()
     
 
 
@@ -10,9 +13,9 @@ class Articulo(models.Model):
 
 
 class Venta(models.Model):
-    codVenta = models.IntegerField(null=True,
-                                blank=True)
+    codVenta = models.AutoField(primary_key=True)
     cantVenta= models.IntegerField(null=True)
     fechaVenta= models.DateTimeField(null=True,
                                     auto_now_add=True)
+    articulo= models.OneToOneField(Articulo, on_delete=models.CASCADE)
     
