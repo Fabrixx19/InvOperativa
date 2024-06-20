f0e69744ccfd420b897a796d1f868c49e332dc1c
Fabrizio Avallone <143529524+Fabrixx19@users.noreply.github.com>
2024-06-20 15:08:30 -0300

    sd

--- invOperativa/stock/admin.py
+++ invOperativa/stock/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Articulo, Venta, OrdenDeCompra, EstadoArticulo, EstadoOrdenCompra, ModeloInventario, Proveedor, Prediccion_Demanda, Demanda, ArticuloProveedor, MetodoError
+from .models import Articulo, Venta, OrdenDeCompra, EstadoArticulo, EstadoOrdenCompra, ModeloInventario, Proveedor, Prediccion_Demanda, Demanda, MetodoError
 # Register your models here.
 
 admin.site.register(Articulo)
@@ -10,6 +10,5 @@ admin.site.register(Demanda)
 admin.site.register(Prediccion_Demanda)
 admin.site.register(EstadoOrdenCompra)
 admin.site.register(EstadoArticulo)
-admin.site.register(ArticuloProveedor)
 admin.site.register(ModeloInventario)
 admin.site.register(MetodoError)
